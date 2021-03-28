92e6489fc5440d35ac13a2ea1e21face945a3b6d
Dongin Kim <miknignod@gmail.com>
2021-03-29 00:47:43 +0900

    쿠키 적용

--- routes/auth/check.js
+++ routes/auth/check.js
@@ -1,50 +1,64 @@
 const express = require('express');
 const passport = require('passport');
-const { Follow, Post, User } = require('../../models');
+const { User } = require('../../models');
 
 const router = express.Router();
-
 router.get(
   '/',
-  (req, res, next) => {
-    if (req.headers.authorization)
-      return passport.authenticate('jwt', { session: false })(req, res, next);
-    return res.status(200).end();
-  },
+  passport.authenticate('jwt', { session: false }),
   async (req, res) => {
-    const userPromise = User.findOne({
+    const user = await User.findOne({
       where: { id: req.user.id },
-      attributes: ['email', 'id', 'nickname', 'createdAt'],
+      attributes: ['createdAt', 'email', 'id', 'nickname'],
       raw: true,
     });
-    const countPromise = Post.count({
-      where: {
-        UserId: req.user.id,
-      },
+    res.json({
+      User: user,
+      isLoggedIn: true,
     });
-    const followerPromise = Follow.count({
-      where: {
-        FollowingId: req.user.id,
-      },
-    });
-    const followingPromise = Follow.count({
-      where: {
-        FollowerId: req.user.id,
-      },
-    });
-
-    const [user, count, erCount, ingCout] = await Promise.all([
-      userPromise,
-      countPromise,
-      followerPromise,
-      followingPromise,
-    ]);
-    user.PostsCount = count;
-    user.FollowerCount = erCount;
-    user.FollowingCount = ingCout;
-
-    res.json({ User: user });
   },
 );
+// router.get(
+//   '/',
+//   (req, res, next) => {
+//     if (req.headers.authorization)
+//       return passport.authenticate('jwt', { session: false })(req, res, next);
+//     return res.status(200).end();
+//   },
+//   async (req, res) => {
+//     const userPromise = User.findOne({
+//       where: { id: req.user.id },
+//       attributes: ['email', 'id', 'nickname', 'createdAt'],
+//       raw: true,
+//     });
+//     const countPromise = Post.count({
+//       where: {
+//         UserId: req.user.id,
+//       },
+//     });
+//     const followerPromise = Follow.count({
+//       where: {
+//         FollowingId: req.user.id,
+//       },
+//     });
+//     const followingPromise = Follow.count({
+//       where: {
+//         FollowerId: req.user.id,
+//       },
+//     });
+
+//     const [user, count, erCount, ingCout] = await Promise.all([
+//       userPromise,
+//       countPromise,
+//       followerPromise,
+//       followingPromise,
+//     ]);
+//     user.PostsCount = count;
+//     user.FollowerCount = erCount;
+//     user.FollowingCount = ingCout;
+
+//     res.json({ User: user });
+//   },
+// );
 
 module.exports = router;
--- routes/posts/index.js
+++ routes/posts/index.js
@@ -1,47 +1,98 @@
 const express = require('express');
 const passport = require('passport');
 const { Op } = require('sequelize');
-const { Image, Post, User, Follow } = require('../../models');
+const { Image, Post, User, Follow, Like } = require('../../models');
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  console.log('COOKIE', req.cookies);
-  const lastId = parseInt(req.query.lastId, 10);
-  const where = {};
+router.get(
+  '/',
+  (req, res, next) => {
+    passport.authenticate(
+      'jwt',
+      { session: false },
+      async (err, user, info) => {
+        if (err) return next(err);
+        if (info) return next();
 
-  if (lastId) {
-    where.id = { [Op.lt]: lastId };
-  }
+        const lastId = parseInt(req.query.lastId, 10);
+        const where = {};
 
-  const posts = await Post.findAll({
-    where,
-    limit: 10,
-    order: [['createdAt', 'DESC']],
-    include: [
-      { model: Image },
-      { model: User, attributes: ['id', 'nickname'] },
-    ],
-  });
+        if (lastId) {
+          where.id = { [Op.lt]: lastId };
+        }
 
-  // const users = posts.reduce((acc, cur) => {
-  //   if (!acc[cur.UserId]) acc[cur.UserId] = cur.User;
-  //   return acc;
-  // }, {});
+        const posts = await Post.findAll({
+          where,
+          limit: 10,
+          order: [['createdAt', 'DESC']],
+          include: [
+            { model: Image },
+            { model: User, attributes: ['id', 'nickname'] },
+          ],
+        });
 
-  // const result = await Promise.all(
-  //   Object.keys(users).map((UserId) => {
-  //     return Follow.findOne({
-  //       where: {
-  //         FollowingId: UserId,
-  //         FollowerId: req.user.id,
-  //       },
-  //     });
-  //   }),
-  // );
+        const likePosts = await Promise.all(
+          posts.map((post) => {
+            return Like.findOne({
+              where: {
+                PostId: post.id,
+                UserId: user.id,
+              },
+            });
+          }),
+        );
 
-  return res.json({ Posts: posts });
-});
+        likePosts.forEach((like, index) => {
+          if (like) {
+            posts[index].dataValues.isLiked = true;
+          }
+        });
+
+        const users = posts.reduce((acc, cur) => {
+          if (!acc[cur.UserId]) acc[cur.UserId] = cur.User;
+          return acc;
+        }, {});
+
+        const result = await Promise.all(
+          Object.keys(users).map((id) => {
+            return Follow.findOne({
+              where: {
+                FollowingId: id,
+                FollowerId: user.id,
+              },
+            });
+          }),
+        );
+        result.forEach((cur) => {
+          if (cur) users[cur.FollowingId].dataValues.isFollowing = true;
+        });
+
+        return res.json({ Posts: posts, users });
+      },
+    )(req, res, next);
+  },
+  async (req, res) => {
+    const lastId = parseInt(req.query.lastId, 10);
+    const where = {};
+
+    if (lastId) {
+      where.id = { [Op.lt]: lastId };
+    }
+
+    const posts = await Post.findAll({
+      where,
+      limit: 10,
+      order: [['createdAt', 'DESC']],
+      include: [
+        { model: Image },
+        { model: User, attributes: ['id', 'nickname'] },
+      ],
+    });
+
+    return res.json({ Posts: posts });
+  },
+);
 
 // 쿠키 사용하면 필요없어질 듯
 router.get(
@@ -78,20 +129,6 @@ router.get(
     });
 
     return res.json({ Likes: posts, users });
-  },
-);
-router.get(
-  '/update',
-  passport.authenticate('jwt', { session: false }),
-  async (req, res) => {
-    const posts = await Post.findAll({
-      limit: 10,
-      order: [['createdAt', 'DESC']],
-      attributes: ['id', 'createdAt'],
-      include: [{ model: User, as: 'Likers', where: { id: req.user.id } }],
-    });
-
-    return res.json({ Likes: posts });
   },
 );
 
